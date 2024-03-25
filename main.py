33206900fe0129df821d2aaa98c4fd85492cc3a4
srikrishnadinda018 <127649098+srikrishnadinda018@users.noreply.github.com>
2024-03-25 20:22:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,111 +50,27 @@ with st.sidebar:
 
 if (selected == 'Heart Disease Prediction'):
 
-    
-
     # page title
 
     st.title('Heart Disease Prediction using ML')
-
+    age = st.text_input('Age')
+    sex = st.text_input('Sex')
+    cp = st.text_input('Chest Pain types')
+    trestbps = st.text_input('Resting Blood Pressure')
+    chol = st.text_input('Serum Cholestoral in mg/dl')
+    fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+    restecg = st.text_input('Resting Electrocardiographic results')
+    thalach = st.text_input('Maximum Heart Rate achieved')
+    exang = st.text_input('Exercise Induced Angina')
+    oldpeak = st.text_input('ST depression induced by exercise')
+    slope = st.text_input('Slope of the peak exercise ST segment')
+    ca = st.text_input('Major vessels colored by flourosopy')
+    thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
     
-
-    col1, col2, col3 = st.columns(3)
-
-    
-
-    with col1:
-
-        age = st.text_input('Age')
-
-        
-
-    with col2:
-
-        sex = st.text_input('Sex')
-
-        
-
-    with col3:
-
-        cp = st.text_input('Chest Pain types')
-
-        
-
-    with col1:
-
-        trestbps = st.text_input('Resting Blood Pressure')
-
-        
-
-    with col2:
-
-        chol = st.text_input('Serum Cholestoral in mg/dl')
-
-        
-
-    with col3:
-
-        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
-
-        
-
-    with col1:
-
-        restecg = st.text_input('Resting Electrocardiographic results')
-
-        
-
-    with col2:
-
-        thalach = st.text_input('Maximum Heart Rate achieved')
-
-        
-
-    with col3:
-
-        exang = st.text_input('Exercise Induced Angina')
-
-        
-
-    with col1:
-
-        oldpeak = st.text_input('ST depression induced by exercise')
-
-        
-
-    with col2:
-
-        slope = st.text_input('Slope of the peak exercise ST segment')
-
-        
-
-    with col3:
-
-        ca = st.text_input('Major vessels colored by flourosopy')
-
-        
-
-    with col1:
-
-        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
-
-        
-
-        
-
-     
-
-     
-
     # code for Prediction
-
     heart_diagnosis = ''
 
-    
-
     # creating a button for Prediction
-
-    
 
     if st.button('Heart Disease Test Result'):
 
@@ -163,11 +79,9 @@ if (selected == 'Heart Disease Prediction'):
         
 
         if (heart_prediction[0] == 1):
-
           heart_diagnosis = 'The person is having heart disease'
 
         else:
-
           heart_diagnosis = 'The person does not have any heart disease'
 
         
