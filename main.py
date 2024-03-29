9131c69030025c5d4e87a7e68a5bb3e22febdf9f
srikrishnadinda018 <127649098+srikrishnadinda018@users.noreply.github.com>
2024-03-29 09:21:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,19 +54,19 @@ if (selected == 'HEART DISEASE PREDICTION'):
     # page title
 
     st.title('Heart Disease Prediction using ML')
-    age = st.number_input('Age')
-    sex = st.number_input('Sex')
-    cp = st.number_input('Chest Pain types')
-    trestbps = st.number_input('Resting Blood Pressure')
-    chol = st.number_input('Serum Cholestoral in mg/dl')
-    fbs = st.number_input('Fasting Blood Sugar > 120 mg/dl')
-    restecg = st.number_input('Resting Electrocardiographic results')
-    thalach = st.number_input('Maximum Heart Rate achieved')
-    exang = st.number_input('Exercise Induced Angina')
+    age = st.number_input('Age',step=1)
+    sex = st.number_input('Sex',step=1)
+    cp = st.number_input('Chest Pain types',step=1)
+    trestbps = st.number_input('Resting Blood Pressure',step=1)
+    chol = st.number_input('Serum Cholestoral in mg/dl',step=1)
+    fbs = st.number_input('Fasting Blood Sugar > 120 mg/dl',step=1)
+    restecg = st.number_input('Resting Electrocardiographic results',step=1)
+    thalach = st.number_input('Maximum Heart Rate achieved',step=1)
+    exang = st.number_input('Exercise Induced Angina',step=1)
     oldpeak = st.number_input('ST depression induced by exercise')
-    slope = st.number_input('Slope of the peak exercise ST segment')
-    ca = st.number_input('Major vessels colored by flourosopy')
-    thal = st.number_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+    slope = st.number_input('Slope of the peak exercise ST segment',step=1)
+    ca = st.number_input('Major vessels colored by flourosopy',step=1)
+    thal = st.number_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect',step=1)
     
     # code for Prediction
     heart_diagnosis = ''
